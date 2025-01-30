cf5ae97351a584b1b1586a45357bb05e9b20d31f
ROUNAK GUPTA <rounakg866@gmail.com>
2025-01-30 18:11:57 +0530

    changes

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
-import { About } from "./components/about";
+import { About } from "./components/About";
 import { Skills } from "./components/Skills";
 import { LeetCode } from "./components/LeetCode";
 import { Projects } from "./components/Projects";
